017eda43533c6f57f1b65fc1192d0d6d1190b98b
Matthew Monroe <Matthew.Monroe@pnnl.gov>
2010-07-09 19:29:41 +0000

    Minor installer updates

--- MSFileInfoScanner_SourceCode/SourceAndSupportingDLLs/Update_Supporting_Source_Code.bat
+++ MSFileInfoScanner_SourceCode/SourceAndSupportingDLLs/Update_Supporting_Source_Code.bat
@@ -1,5 +1,4 @@
 @echo on
-Del PRISM_Class_Library_Source*.zip
 Del SharedVBNetRoutines_Source*.zip
 Del NetCDFReader_Source*.zip
 Del SharpZipLib_Source*.zip
@@ -7,7 +6,6 @@ Del SharpZipLib_Source*.zip
 @echo off
 echo;
 
-Copy "F:\My Documents\Projects\DataMining\DMS_Managers\PRISM_Class_Library\PRISM_Class_Library_Source*.zip" .
 Copy "F:\My Documents\Projects\DataMining\SharedVBNetRoutines\SharedVBNetRoutines_SourceCode\*.zip" .
 Copy "F:\My Documents\Projects\DataMining\NETCDF.NET\NetCDFReader_SourceCode\*.zip" .
 Copy "F:\My Documents\Projects\DataMining\SharpZipLib.NET\SharpZipLib_SourceCode\*.zip" .
