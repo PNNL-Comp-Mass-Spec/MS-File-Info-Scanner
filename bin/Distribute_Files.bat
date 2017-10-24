25749db2b62aae16e2f0ff3b17f710ce02d86552
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-24 14:33:37 -0700

    Update the distribution batch file

--- bin/Distribute_Files.bat
+++ bin/Distribute_Files.bat
@@ -14,16 +14,27 @@ xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\SpectraTypeC
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoScannerInterfaces.*"   \\floyd\Software\MSFileInfoScanner\Exe_Only /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.txt"                 \\floyd\Software\MSFileInfoScanner\Exe_Only /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\UIMFLibrary.*"                   \\floyd\Software\MSFileInfoScanner\Exe_Only /y /d
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MathNet.Numerics.*"                   \\floyd\Software\MSFileInfoScanner\Exe_Only /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MathNet.Numerics.*"              \\floyd\Software\MSFileInfoScanner\Exe_Only /y /d
 
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.dll"       c:\dms_programs\MSFileInfoScanner /y
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.xml"       c:\dms_programs\MSFileInfoScanner /y
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.pdb"       c:\dms_programs\MSFileInfoScanner /y
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\SpectraTypeClassifier.dll"       c:\dms_programs\MSFileInfoScanner /y
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoScannerInterfaces.*"   c:\dms_programs\MSFileInfoScanner /y
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.txt"                 c:\dms_programs\MSFileInfoScanner /y
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\UIMFLibrary.*"                   c:\dms_programs\MSFileInfoScanner /y /d
-xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MathNet.Numerics.*"                   c:\dms_programs\MSFileInfoScanner /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.dll"           c:\dms_programs\MSFileInfoScanner /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.xml"           c:\dms_programs\MSFileInfoScanner /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.pdb"           c:\dms_programs\MSFileInfoScanner /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\SpectraTypeClassifier.dll"       c:\dms_programs\MSFileInfoScanner /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScannerInterfaces.*"   c:\dms_programs\MSFileInfoScanner /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.txt"                     c:\dms_programs\MSFileInfoScanner /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\UIMFLibrary.*"                   c:\dms_programs\MSFileInfoScanner /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MathNet.Numerics.*"              c:\dms_programs\MSFileInfoScanner /y /d
+
+
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoScanner.exe"           C:\DMS_Programs\MSFileInfoScannerExe /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoScanner.xml"           C:\DMS_Programs\MSFileInfoScannerExe /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoScanner.pdb"           C:\DMS_Programs\MSFileInfoScannerExe /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\SpectraTypeClassifier.dll"       C:\DMS_Programs\MSFileInfoScannerExe /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoScannerInterfaces.*"   C:\DMS_Programs\MSFileInfoScannerExe /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.txt"                 C:\DMS_Programs\MSFileInfoScannerExe /y
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\UIMFLibrary.*"                   C:\DMS_Programs\MSFileInfoScannerExe /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MathNet.Numerics.*"              C:\DMS_Programs\MSFileInfoScannerExe /y /d
+
 
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.dll"           \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MSFileInfoScanner /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.pdb"           \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MSFileInfoScanner /y
