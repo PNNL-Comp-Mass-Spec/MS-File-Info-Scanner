97dc9d9e0d717851ed88cddd95582ed69fcdd460
alchemistmatt <matthew.monroe@pnnl.gov>
2017-11-04 14:47:31 -0700

    Update the distribution batch file to include PRISM.dll and ThermoRawFileReader

--- bin/Distribute_Files.bat
+++ bin/Distribute_Files.bat
@@ -15,6 +15,8 @@ xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoSc
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.txt"                 \\floyd\Software\MSFileInfoScanner\Exe_Only /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\UIMFLibrary.*"                   \\floyd\Software\MSFileInfoScanner\Exe_Only /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MathNet.Numerics.*"              \\floyd\Software\MSFileInfoScanner\Exe_Only /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\PRISM.dll"                   \\floyd\Software\MSFileInfoScanner\Exe_Only /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\ThermoRawFileReader.*"       \\floyd\Software\MSFileInfoScanner\Exe_Only /y /d
 
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.dll"           c:\dms_programs\MSFileInfoScanner /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.xml"           c:\dms_programs\MSFileInfoScanner /y
@@ -24,6 +26,8 @@ xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileIn
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.txt"                     c:\dms_programs\MSFileInfoScanner /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\UIMFLibrary.*"                   c:\dms_programs\MSFileInfoScanner /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MathNet.Numerics.*"              c:\dms_programs\MSFileInfoScanner /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\PRISM.dll"                       c:\dms_programs\MSFileInfoScanner /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\ThermoRawFileReader.*"           c:\dms_programs\MSFileInfoScanner /y /d
 
 
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoScanner.exe"           C:\DMS_Programs\MSFileInfoScannerExe /y
@@ -34,7 +38,8 @@ xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoSc
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.txt"                 C:\DMS_Programs\MSFileInfoScannerExe /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\UIMFLibrary.*"                   C:\DMS_Programs\MSFileInfoScannerExe /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MathNet.Numerics.*"              C:\DMS_Programs\MSFileInfoScannerExe /y /d
-
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\PRISM.dll"                   c:\dms_programs\MSFileInfoScannerExe /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\ThermoRawFileReader.*"       c:\dms_programs\MSFileInfoScannerExe /y /d
 
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.dll"           \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MSFileInfoScanner /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.pdb"           \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MSFileInfoScanner /y
@@ -45,6 +50,7 @@ xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\UIMFLibrary.*"                   \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MSFileInfoScanner /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MathNet.Numerics.*"              \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MSFileInfoScanner /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\PRISM.dll"                       \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MSFileInfoScanner /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\ThermoRawFileReader.*"           \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution\MSFileInfoScanner /y /d
 
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.dll"           \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution_NewDecon2LS\MSFileInfoScanner /y
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScanner.pdb"           \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution_NewDecon2LS\MSFileInfoScanner /y
@@ -55,6 +61,8 @@ xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\UIMFLibrary.*"                   \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution_NewDecon2LS\MSFileInfoScanner /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MathNet.Numerics.*"              \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution_NewDecon2LS\MSFileInfoScanner /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\PRISM.dll"                       \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution_NewDecon2LS\MSFileInfoScanner /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\ThermoRawFileReader.*"           \\pnl\projects\OmicsSW\DMS_Programs\AnalysisToolManagerDistribution_NewDecon2LS\MSFileInfoScanner /y /d
+pause
 
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScannerInterfaces.dll"   \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution /y
 
@@ -67,5 +75,6 @@ xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\RevisionHistory.
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\UIMFLibrary.*"                   \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\MSFileInfoScanner /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MathNet.Numerics.*"              \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\MSFileInfoScanner /y /d
 xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\PRISM.dll"                       \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\MSFileInfoScanner /y /d
+xcopy "F:\My Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\ThermoRawFileReader.*"           \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\MSFileInfoScanner /y /d
 
 pause
