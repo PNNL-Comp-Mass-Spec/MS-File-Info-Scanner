e588be05fade8776785c0024f11b5f68bc0fe127
alchemistmatt <matthew.monroe@pnnl.gov>
2018-10-15 19:19:10 -0700

    Updated PRISM.dll from NuGet

--- modMain.cs
+++ modMain.cs
@@ -22,7 +22,7 @@ namespace MSFileInfoScanner
     static class modMain
     {
 
-        public const string PROGRAM_DATE = "October 10, 2018";
+        public const string PROGRAM_DATE = "October 15, 2018";
 
         // This path can contain wildcard characters, e.g. C:\*.raw
         private static string mInputDataFilePath;
