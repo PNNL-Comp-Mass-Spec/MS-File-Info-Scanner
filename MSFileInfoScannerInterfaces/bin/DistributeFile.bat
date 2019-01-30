dc599c4f906b50028b0e7f5cff1fc2c49a36fb8b
alchemistmatt <matthew.monroe@pnnl.gov>
2019-01-30 12:24:43 -0800

    Add batch file

---
+++ MSFileInfoScannerInterfaces/bin/DistributeFile.bat
@@ -0,0 +1,89 @@
+F:
+cd F:\Documents\Projects\DataMining\MS_File_Info_Scanner\MSFileInfoScannerInterfaces\bin\
+
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\CodeTest\Lib\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\CodeTest\bin\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\CodeTest\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\CodeTest\bin\Release\MSFileInfoScannerInterfaces.dll" /Y /D
+
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Common\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\AM_Shared\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\AM_Shared\bin\Mono\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\AM_Shared\bin\Release\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AM_Program\bin\Release\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\AnalysisManagerTest\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Ape_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_AScore_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Bruker_DA_Export\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Cyclops_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DataImport_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Decon2ls_PlugIn_Decon2LSV2\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DeconPeakDetector_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DTA_Import_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DTA_Split_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DtaRefinery_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_DTASpectraFileGen_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Extraction_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Formularity\bin\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_GlyQIQ_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_ICR2LS_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_IDM_Plugin\AM_IDM_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_IDPicker_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_InSpecT_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_InspectResultsAssembly_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_LCMSFeatureFinder_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_LipidMapSearch_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Mage_PlugIn\bin\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Masic_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MetaboliteDetector_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MODa_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MODPlus_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSAlign_Histone_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSAlign_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSAlign_Quant_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSDeconv_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSGF_PlugIn\Bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSGFDB_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSGFDB_PlugIn\MSGFPlusIndexFileCopier\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSMSSpectrumFilter_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSPathFinder_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSXML_Bruker_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MSXML_Gen_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MultiAlign_Aggregator_PlugIn\bin\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_MultiAlign_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Mz_Refinery_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_NOMSI\bin\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_OMSSA_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_PBFSpectraFileGen_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Phospho_FDR_Aggregator_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_PRIDE_Converter_PlugIn\bin\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_PRIDE_MzXML_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_ProMex_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_ProSightPC_Quant_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_QC-ART_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_RepoPkgr_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_ResultsCleanup_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_ResultsXfer_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_Sequest_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_SMAQC_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_ThermoPeakDataExporter\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_TopFD_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_TopPIC_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_UIMFtoMassHunter_Plugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Plugins\AM_XTandem_PlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestApePlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestAScorePlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Analysis_Manager\Test_Plugins\TestMultiAlignPlugIn\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\RefLib\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\DeployedFiles\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\CaptureTaskManager\CaptureTaskManager\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\CaptureTaskManager\CTM_Shared\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\DMS_Managers\Capture_Task_Manager\DatasetInfoPlugin\DatasetInfoPlugin\bin\Debug\MSFileInfoScannerInterfaces.dll" /Y /D
+
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\Lib\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\DLL\MSFileInfoScannerInterfaces.dll" /Y /D
+xcopy MSFileInfoScannerInterfaces.dll "F:\Documents\Projects\DataMining\MS_File_Info_Scanner\bin\MSFileInfoScannerInterfaces.dll" /Y /D
+
+pause
