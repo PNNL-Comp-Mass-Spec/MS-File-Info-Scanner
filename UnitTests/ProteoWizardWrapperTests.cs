9989db28d2695687ba230769cf2cc7d598ecd6cb
alchemistmatt <matthew.monroe@pnnl.gov>
2019-05-15 17:28:57 -0700

    Update ProteoWizard test method to use ProteowizardWrapper instead of directly interacting with ProteoWizard

--- UnitTests/ProteoWizardWrapperTests.cs
+++ UnitTests/ProteoWizardWrapperTests.cs
@@ -1,27 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using MSFileInfoScanner;
 using NUnit.Framework;
 using PRISM;
-using pwiz.CLI.msdata;
 
 // ReSharper disable StringLiteralTypo
 namespace MSFileInfoScannerUnitTests
 {
+    /// <summary>
+    /// Test reading data with ProteoWizard
+    /// </summary>
+    /// <remarks>
+    /// SetUpFixture ProteoWizardSetup looks for ProteoWizard prior to this class loading
+    /// </remarks>
     [TestFixture]
     class ProteoWizardWrapperTests
     {
 
         [Test]
-        [TestCase("Angiotensin_325-ETD.raw", false)]
-        [TestCase("Angiotensin_325-ETciD-15.raw", false)]
-        [TestCase("Angiotensin_325-HCD.raw", false)]
-        [TestCase("Angiotensin_325-CID.raw", false)]
-        [TestCase("PPS20190130US1-1_1030TRANCID35.raw", true)]
-        public void TestPWiz(string fileOrDirectoryName, bool isDirectory)
+        [TestCase("Angiotensin_325-CID.raw", false, 10, 10, 26390, 1.4326E+008, 4.2483E+006)]
+        [TestCase("Angiotensin_325-ETD.raw", false, 10, 10, 31505, 1.4025E+008, 2.6627E+006)]
+        [TestCase("Angiotensin_325-ETciD-15.raw", false, 10, 10, 30883, 1.6188E+008, 3.0563E+006)]
+        [TestCase("Angiotensin_325-HCD.raw", false, 10, 10, 36158, 8.3286E+007, 2.2395E+006)]
+        [TestCase("Angiotensin_AllScans.raw", false, 1775, 51, 25782, 1.0861E+009, 1.2757E+008)]
+        [TestCase("PPS20190130US1-1_1030TRANCID35.raw", true, 11600, 71, 241, 37.085, 6.8873)]
+        public void TestPWiz(
+            string fileOrDirectoryName,
+            bool isDirectory,
+            int expectedSpectraInFile,
+            int expectedSpectraLoaded,
+            int expectedTotalDataPoints,
+            double expectedMedianTIC,
+            double expectedMedianBPI)
         {
-            const bool RUN_BENCHMARKS = false;
-
             try
             {
                 if (!FindInstrumentData(fileOrDirectoryName, isDirectory, out var datasetPath))
@@ -30,137 +43,165 @@ namespace MSFileInfoScannerUnitTests
                     return;
                 }
 
-                var pWiz2 = new MSDataFile(datasetPath);
+                var pWiz = new pwiz.ProteowizardWrapper.MSDataFileReader(datasetPath);
 
-                Console.WriteLine("Spectrum count: " + pWiz2.run.spectrumList.size());
+
+                Console.WriteLine("Chromatogram count: " + pWiz.ChromatogramCount);
                 Console.WriteLine();
 
-                if (pWiz2.run.spectrumList.size() > 0)
+                var ticIntensities = new Dictionary<int, float>();
+
+                for (var chromatogramIndex = 0; chromatogramIndex < pWiz.ChromatogramCount; chromatogramIndex++)
                 {
-                    var spectrumIndex = 0;
+                    // Note that even for a small .Wiff file (1.5 MB), obtaining the Chromatogram list will take some time (20 to 60 seconds)
+                    // The chromatogram at index 0 should be the TIC
+                    // The chromatogram at index >=1 will be each SRM
 
-                    do
+                    pWiz.GetChromatogram(chromatogramIndex, out var chromatogramID, out var timeArray, out var intensityArray);
+
+                    // Determine the chromatogram type
+
+                    if (chromatogramID == null)
+                        chromatogramID = string.Empty;
+
+                    var cvParams = pWiz.GetChromatogramCVParams(chromatogramIndex);
+
+                    if (clsProteoWizardDataParser.TryGetCVParam(cvParams, pwiz.CLI.cv.CVID.MS_TIC_chromatogram, out _))
                     {
-                        var spectrum = pWiz2.run.spectrumList.spectrum(spectrumIndex, getBinaryData: true);
+                        // This chromatogram is the TIC
+                        Console.WriteLine("TIC has id {0} and {1} data points", chromatogramID, timeArray.Length);
 
-                        pwiz.CLI.data.CVParam param;
-                        if (spectrum.scanList.scans.Count > 0)
+                        for (var i = 0; i < intensityArray.Length; i++)
                         {
-                            if (clsProteoWizardDataParser.TryGetCVParam(spectrum.scanList.scans[0].cvParams, pwiz.CLI.cv.CVID.MS_scan_start_time, out param))
-                            {
-                                var scanNum = spectrumIndex + 1;
-                                var startTimeMinutes = param.timeInSeconds() / 60.0;
-
-                                Console.WriteLine("ScanIndex " + spectrumIndex + ", Scan " + scanNum + ", Elution Time " + startTimeMinutes + " minutes");
-                            }
-
+                            ticIntensities.Add(i, intensityArray[i]);
                         }
+                    }
 
-                        // Use the following to determine info on this spectrum
-                        if (clsProteoWizardDataParser.TryGetCVParam(spectrum.cvParams, pwiz.CLI.cv.CVID.MS_ms_level, out param))
-                        {
-                            int.TryParse(param.value, out _);
-                        }
+                    if (clsProteoWizardDataParser.TryGetCVParam(cvParams, pwiz.CLI.cv.CVID.MS_selected_reaction_monitoring_chromatogram, out _))
+                    {
+                        // This chromatogram is an SRM scan
+                        Console.WriteLine("SRM scan has id {0} and {1} data points", chromatogramID, timeArray.Length);
+                    }
 
-                        // Use the following to get the MZs and Intensities
-                        var mzList = spectrum.getMZArray();
-                        spectrum.getIntensityArray();
-
-                        if (mzList.data.Count > 0)
-                        {
-                            Console.WriteLine("  Data count: " + mzList.data.Count);
-
-                            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
-                            if (RUN_BENCHMARKS)
-                            {
-                                double tic1 = 0;
-                                double tic2 = 0;
-                                var startTime = default(DateTime);
-                                var endTime = default(DateTime);
-                                double runTimeSeconds1 = 0;
-                                double runTimeSeconds2 = 0;
-                                const int LOOP_ITERATIONS = 2000;
-
-                                // Note from Matt Chambers (matt.chambers42 at GMail)
-                                // Repeatedly accessing items directly via mzList.data() can be very slow
-                                // With 700 points and 2000 iterations, it takes anywhere from 0.6 to 1.1 seconds to run from startTime to endTime
-                                startTime = DateTime.Now;
-                                for (var j = 1; j <= LOOP_ITERATIONS; j++)
-                                {
-                                    for (var index = 0; index <= mzList.data.Count - 1; index++)
-                                    {
-                                        tic1 += mzList.data[index];
-                                    }
-                                }
-                                endTime = DateTime.Now;
-                                runTimeSeconds1 = endTime.Subtract(startTime).TotalSeconds;
-
-                                // The preferred method is to copy the data from .data to a locally-stored mzArray var
-                                // With 700 points and 2000 iterations, it takes 0.016 seconds to run from startTime to endTime
-                                startTime = DateTime.Now;
-                                for (var j = 1; j <= LOOP_ITERATIONS; j++)
-                                {
-                                    var mzArray = mzList.data;
-                                    for (var index = 0; index <= mzArray.Count - 1; index++)
-                                    {
-                                        tic2 += mzArray[index];
-                                    }
-                                }
-                                endTime = DateTime.Now;
-                                runTimeSeconds2 = endTime.Subtract(startTime).TotalSeconds;
-
-                                Console.WriteLine("  " + mzList.data.Count + " points with " + LOOP_ITERATIONS + " iterations gives Runtime1=" + runTimeSeconds1.ToString("0.0##") + " sec. vs. Runtime2=" + runTimeSeconds2.ToString("0.0##") + " sec.");
-
-                                if (Math.Abs(tic1 - tic2) > float.Epsilon)
-                                {
-                                    Console.WriteLine("  TIC values don't agree; this is unexpected");
-                                }
-                            }
-
-                        }
-
-                        if (spectrumIndex < 25)
-                        {
-                            spectrumIndex += 1;
-                        }
-                        else
-                        {
-                            spectrumIndex += 50;
-                        }
-
-                    } while (spectrumIndex < pWiz2.run.spectrumList.size());
                 }
 
-                if (pWiz2.run.chromatogramList.size() > 0)
+                Console.WriteLine("Spectrum count: " + pWiz.SpectrumCount);
+
+                var spectraLoaded = 0;
+                long totalPointsRead = 0;
+                double ticSumAllSpectra = 0;
+                double bpiSumAllSpectra = 0;
+
+                var spectrumIndex = 0;
+                while (spectrumIndex < pWiz.SpectrumCount)
                 {
-                    var chromatogramIndex = 0;
+                    var spectrum = pWiz.GetSpectrum(spectrumIndex, getBinaryData: true);
+                    spectraLoaded += 1;
 
-                    do
+                    Console.WriteLine();
+                    Console.WriteLine("ScanIndex {0}, NativeId {1}, Elution Time {2:F2} minutes, MS Level {3}",
+                                      spectrumIndex, spectrum.NativeId, spectrum.RetentionTime, spectrum.Level);
+
+                    // Use the following to get the MZs and Intensities
+                    var mzList = spectrum.Mzs.ToList();
+                    var intensities = spectrum.Intensities.ToList();
+
+                    if (mzList.Count > 0)
                     {
-                        var timeIntensityPairList = new TimeIntensityPairList();
 
-                        // Note that even for a small .Wiff file (1.5 MB), obtaining the Chromatogram list will take some time (20 to 60 seconds)
-                        // The chromatogram at index 0 should be the TIC
-                        // The chromatogram at index >=1 will be each SRM
+                        Console.WriteLine("  Data count: " + mzList.Count);
 
-                        var chromatogram = pWiz2.run.chromatogramList.chromatogram(chromatogramIndex, getBinaryData: true);
+                        totalPointsRead += mzList.Count;
 
-                        // Determine the chromatogram type
-
-                        if (clsProteoWizardDataParser.TryGetCVParam(chromatogram.cvParams, pwiz.CLI.cv.CVID.MS_TIC_chromatogram, out var param))
+                        double tic = 0;
+                        double bpi = 0;
+                        for (var index = 0; index <= mzList.Count - 1; index++)
                         {
-                            // Obtain the data
-                            chromatogram.getTimeIntensityPairs(ref timeIntensityPairList);
+                            tic += intensities[index];
+                            if (intensities[index] > bpi)
+                                bpi = intensities[index];
                         }
 
-                        if (clsProteoWizardDataParser.TryGetCVParam(chromatogram.cvParams, pwiz.CLI.cv.CVID.MS_selected_reaction_monitoring_chromatogram, out param))
+                        ticSumAllSpectra += tic;
+                        bpiSumAllSpectra += bpi;
+
+                        if (!ticIntensities.TryGetValue(spectrumIndex, out var ticFromChromatogram))
                         {
-                            // Obtain the SRM scan
-                            chromatogram.getTimeIntensityPairs(ref timeIntensityPairList);
+                            ticFromChromatogram = -1;
                         }
 
-                        chromatogramIndex += 1;
-                    } while (chromatogramIndex < 50 && chromatogramIndex < pWiz2.run.chromatogramList.size());
+                        var spectrumInfo = pWiz.GetSpectrumObject(spectrumIndex);
+
+                        if (clsProteoWizardDataParser.TryGetCVParamDouble(spectrumInfo.cvParams, pwiz.CLI.cv.CVID.MS_total_ion_current,
+                                                                          out var ticFromSpectrumObject))
+                        {
+                            if (ticFromChromatogram < 0)
+                            {
+                                // ticIntensities did not have an entry for spectrumIndex
+                                // This could be the case on a Waters Synapt instrument, where ticIntensities has one TIC per frame,
+                                // while pWiz.GetSpectrum() returns individual mass spectra, of which there could be hundreds of spectra per frame
+                                Console.WriteLine("  TIC from actual data is {0:E2} vs. {1:E2} from the spectrum object",
+                                                  tic, ticFromSpectrumObject);
+                            }
+                            else
+                            {
+
+                                // Note: the TIC value from the CvParams has been seen to be drastically off from the manually computed value
+                                Console.WriteLine("  TIC from actual data is {0:E2} vs. {1:E2} from the chromatogram and {2:E2} from the spectrum object",
+                                                  tic, ticFromChromatogram, ticFromSpectrumObject);
+                            }
+                        }
+
+                        if (clsProteoWizardDataParser.TryGetCVParamDouble(spectrumInfo.cvParams, pwiz.CLI.cv.CVID.MS_base_peak_intensity,
+                                                                          out var bpiFromSpectrumObject))
+                        {
+                            if (clsProteoWizardDataParser.TryGetCVParamDouble(spectrumInfo.cvParams, pwiz.CLI.cv.CVID.MS_base_peak_m_z,
+                                                                              out var bpiMzFromSpectrumObject))
+                            {
+                                // Note: the BPI intensity from the CvParams has been seen to be drastically off from the manually computed value
+                                Console.WriteLine("  BPI from spectrum object is {0:E2} at {1:F3} m/z",
+                                                  bpiFromSpectrumObject, bpiMzFromSpectrumObject);
+                            }
+                        }
+                    }
+
+                    if (spectrumIndex < 25)
+                    {
+                        spectrumIndex += 1;
+                    }
+                    else if (spectrumIndex < 1250)
+                    {
+                        spectrumIndex += 50;
+                    }
+                    else
+                    {
+                        spectrumIndex += 500;
+                    }
+
+                }
+
+                if (spectraLoaded > 0)
+                {
+                    var medianTIC = ticSumAllSpectra / spectraLoaded;
+                    var medianBPI = bpiSumAllSpectra / spectraLoaded;
+
+                    Console.WriteLine();
+                    Console.WriteLine("Read {0:N0} data points from {1} spectra in {2}",
+                                      totalPointsRead, spectraLoaded, Path.GetFileName(fileOrDirectoryName));
+
+                    Console.WriteLine("Median TIC: {0:E4}", medianTIC);
+                    Console.WriteLine("Median BPI: {0:E4}", medianBPI);
+
+                    Assert.AreEqual(expectedSpectraInFile, pWiz.SpectrumCount, "Total spectrum count mismatch");
+
+                    Assert.AreEqual(expectedSpectraLoaded, spectraLoaded, "Spectra loaded mismatch");
+
+                    Assert.AreEqual(expectedTotalDataPoints, totalPointsRead, "Spectra loaded mismatch");
+                    var ticComparisonTolerance = expectedMedianTIC * 0.01;
+                    var bpiComparisonTolerance = expectedMedianBPI * 0.01;
+
+                    Assert.AreEqual(expectedMedianTIC, medianTIC, ticComparisonTolerance, "Median TIC mismatch");
+                    Assert.AreEqual(expectedMedianBPI, medianBPI, bpiComparisonTolerance, "Median BPI mismatch");
                 }
 
             }
@@ -168,9 +209,7 @@ namespace MSFileInfoScannerUnitTests
             {
                 ConsoleMsgUtils.ShowError("Error using ProteoWizard reader", ex);
             }
-
         }
-
 
         /// <summary>
         /// Look for the file in a directory named Data, in a parent directory to the work directory
