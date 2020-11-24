e32efb8916207801f01d64b9751ab6f0235dd04d
alchemistmatt <matthew.monroe@pnnl.gov>
2020-11-24 13:28:29 -0800

    Remove properties that are now accessible via the Options class

--- MSFileInfoScannerInterfaces/iMSFileInfoScanner.cs
+++ MSFileInfoScannerInterfaces/iMSFileInfoScanner.cs
@@ -63,88 +63,11 @@ namespace MSFileInfoScannerInterfaces
         public virtual string AcquisitionTimeFilename { get; set; }
 
         /// <summary>
-        /// When true, checks the integrity of every file in every directory processed
-        /// </summary>
-        public virtual bool CheckFileIntegrity { get; set; }
-
-        public virtual bool ComputeOverallQualityScores { get; set; }
-
-        /// <summary>
         /// Returns the dataset info, formatted as XML
         /// </summary>
         public virtual string DatasetInfoXML { get; protected set; }
 
-        public virtual bool CheckCentroidingStatus { get; set; }
-
-        /// <summary>
-        /// When True, computes a SHA-1 hash on every file using mFileIntegrityChecker
-        /// </summary>
-        /// <remarks>
-        /// Note, when this is false, the program computes the SHA-1 hash of the primary dataset file (or files),
-        /// unless DisableInstrumentHash is true
-        /// </remarks>
-        public virtual bool ComputeFileHashes { get; set; }
-
-        /// <summary>
-        /// If True, copies .Raw files to the local drive if unable to read the file over the network
-        /// </summary>
-        public virtual bool CopyFileLocalOnReadError { get; set; }
-
-        /// <summary>
-        /// If True, will create the _DatasetInfo.xml file
-        /// </summary>
-        public virtual bool CreateDatasetInfoFile { get; set; }
-
-        /// <summary>
-        /// If True, will create the _ScanStats.txt file
-        /// </summary>
-        public virtual bool CreateScanStatsFile { get; set; }
-
-        /// <summary>
-        /// DatasetID value to use instead of trying to lookup the value in DMS (and instead of using 0)
-        /// </summary>
-        public virtual int DatasetIDOverride { get; set; }
-
-        public virtual string DatasetStatsTextFileName { get; set; }
-
-        public virtual string DSInfoConnectionString { get; set; }
-
-        public virtual bool DSInfoDBPostingEnabled { get; set; }
-
-        public virtual string DSInfoStoredProcedure { get; set; }
-
         public virtual MSFileScannerErrorCodes ErrorCode { get; protected set; }
-
-        public virtual bool IgnoreErrorsWhenRecursing { get; set; }
-
-        public virtual float LCMS2DPlotMZResolution { get; set; }
-
-        public virtual int LCMS2DPlotMaxPointsToPlot { get; set; }
-
-        public virtual int LCMS2DOverviewPlotDivisor { get; set; }
-
-        public virtual int LCMS2DPlotMinPointsPerSpectrum { get; set; }
-
-        public virtual float LCMS2DPlotMinIntensity { get; set; }
-
-        public virtual bool LogMessagesToFile { get; set; }
-
-        public virtual string LogDirectoryPath { get; set; }
-
-        public virtual string LogFilePath { get; set; }
-
-        public virtual int MaximumTextFileLinesToCheck { get; set; }
-
-        public virtual int MaximumXMLElementNodesToCheck { get; set; }
-
-        /// <summary>
-        /// Minimum m/z value that MS/mS spectra should have
-        /// </summary>
-        /// <remarks>
-        /// Useful for validating instrument files where the sample is iTRAQ or TMT labeled
-        /// and it is important to detect the reporter ions in the MS/MS spectra
-        /// </remarks>
-        public virtual float MS2MzMin { get; set; }
 
         /// <summary>
         /// MS2MzMin validation error or warning message
@@ -156,50 +79,6 @@ namespace MSFileInfoScannerInterfaces
         /// </summary>
         public virtual InfoScannerOptions Options { get; protected set; }
 
-        /// <summary>
-        /// When true, generate plots using Python
-        /// </summary>
-        public virtual bool PlotWithPython { get; set; }
-
-        public virtual bool RecheckFileIntegrityForExistingDirectories { get; set; }
-
-        public virtual bool ReprocessExistingFiles { get; set; }
-
-        public virtual bool ReprocessIfCachedSizeIsZero { get; set; }
-
-        /// <summary>
-        /// If True, saves TIC and BPI plots as PNG files
-        /// </summary>
-        public virtual bool SaveTICAndBPIPlots { get; set; }
-
-        /// <summary>
-        /// If True, saves a 2D plot of m/z vs. Intensity (requires reading every data point in the data file, which will slow down the processing)
-        /// </summary>
-        /// <value></value>
-        public virtual bool SaveLCMS2DPlots { get; set; }
-
-        /// <summary>
-        /// When ScanStart is > 0, will start processing at the specified scan number
-        /// </summary>
-        public virtual int ScanStart { get; set; }
-
-        /// <summary>
-        /// When ScanEnd is > 0, will stop processing at the specified scan number
-        /// </summary>
-        public virtual int ScanEnd { get; set; }
-
-        public virtual bool ShowDebugInfo { get; set; }
-
-        public virtual bool UpdateDatasetStatsTextFile { get; set; }
-
-        /// <summary>
-        /// If True, saves/loads data from/to the cache files (DatasetTimeFile.txt and DirectoryIntegrityInfo.txt)
-        /// If you simply want to create TIC and BPI files, and/or the _DatasetInfo.xml file for a single dataset, set this to False
-        /// </summary>
-        /// <remarks>If this is false, data is not loaded/saved from/to the DatasetTimeFile.txt or the DirectoryIntegrityInfo.txt file</remarks>
-        public virtual bool UseCacheFiles { get; set; }
-
-        public virtual bool ZipFileCheckAllData { get; set; }
         public abstract string[] GetKnownDirectoryExtensions();
 
         public abstract string[] GetKnownFileExtensions();
