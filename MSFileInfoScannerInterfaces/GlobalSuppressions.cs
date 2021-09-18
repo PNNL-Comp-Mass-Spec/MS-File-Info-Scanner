d3f9ed79cf98e7146ab1d9da04caded5d7685c1c
alchemistmatt <matthew.monroe@pnnl.gov>
2021-09-17 20:55:46 -0700

    Suppress warnings

--- MSFileInfoScannerInterfaces/GlobalSuppressions.cs
+++ MSFileInfoScannerInterfaces/GlobalSuppressions.cs
@@ -5,4 +5,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "type", Target = "~T:MSFileInfoScannerInterfaces.iMSFileInfoScanner")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allowed name", Scope = "type", Target = "~T:MSFileInfoScannerInterfaces.iMSFileInfoScanner")]
