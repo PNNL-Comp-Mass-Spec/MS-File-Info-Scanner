94cd3af650140469ec8614ccc40ca87385997470
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-12 20:53:39 -0800

    Update program date and cached DLL

--- MSFileInfoScanner.cs
+++ MSFileInfoScanner.cs
@@ -40,7 +40,7 @@ namespace MSFileInfoScanner
     {
         // Ignore Spelling: Bruker, centroiding, idx, Micromass, OxyPlot, Shimadzu, username, utf, yyyy-MM-dd, hh:mm:ss tt, xtr
 
-        public const string PROGRAM_DATE = "November 10, 2022";
+        public const string PROGRAM_DATE = "February 12, 2023";
 
         /// <summary>
         /// Constructor
