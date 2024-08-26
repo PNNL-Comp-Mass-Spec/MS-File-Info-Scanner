2eebca530b1a0bad57392b60fc79bde9a034092c
alchemistmatt <matthew.monroe@pnnl.gov>
2024-08-25 19:50:40 -0700

    Suppress warning

--- SpectraTypeClassifier/GlobalSuppressions.cs
+++ SpectraTypeClassifier/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Usage", "RCS1246:Use element access.", Justification = "Allow for readability", Scope = "member", Target = "~M:SpectraTypeClassifier.SpectrumTypeClassifier.IsDataCentroidedInRegions(System.Collections.Generic.ICollection{System.Double},System.Int32,System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE0305:Simplify collection initialization", Justification = "Leave as-is for readability", Scope = "member", Target = "~M:SpectraTypeClassifier.SpectrumTypeClassifier.CheckSpectrum(System.Int32,System.Double[],System.Int32,SpectraTypeClassifier.SpectrumTypeClassifier.CentroidStatusConstants,System.String)")]
